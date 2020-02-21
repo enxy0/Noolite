c11ac07c77c29b541b4cad26f345e156e8c02b68
enxy0 <enxyox@gmail.com>
2020-02-21 20:15:31 +0300

    Move enum Action to the single file
    Signed-off-by: enxy0 <enxyox@gmail.com>

--- app/src/main/java/com/enxy/noolite/features/model/Script.kt
+++ app/src/main/java/com/enxy/noolite/features/model/Script.kt
@@ -4,13 +4,3 @@ data class Script(
     val name: String,
     val actionsMap: LinkedHashMap<ChannelModel, Action>
 )
-
-enum class Action {
-    TURN_ON,
-    TURN_OFF,
-    TOGGLE_STATE,
-    CHANGE_BRIGHTNESS,
-    CHANGE_COLOR,
-    START_OVERFLOW,
-    STOP_OVERFLOW,
-}
