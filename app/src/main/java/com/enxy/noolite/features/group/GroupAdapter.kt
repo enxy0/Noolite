414c73e60bae0e81784c84b00e2023893584e9e5
enxy0 <enxyox@gmail.com>
2020-02-28 00:43:28 +0300

    Code refactor
    Signed-off-by: enxy0 <enxyox@gmail.com>

--- app/src/main/java/com/enxy/noolite/features/group/GroupAdapter.kt
+++ app/src/main/java/com/enxy/noolite/features/group/GroupAdapter.kt
@@ -3,16 +3,14 @@ package com.enxy.noolite.features.group
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.commit
 import androidx.recyclerview.widget.RecyclerView
 import com.enxy.noolite.R
-import com.enxy.noolite.features.MainViewModel
-import com.enxy.noolite.features.channel.ChannelFragment
 import com.enxy.noolite.features.model.GroupModel
 import kotlinx.android.synthetic.main.item_group.view.*
 
 
-class GroupAdapter(private val fragment: GroupFragment, private val viewModel: MainViewModel) : RecyclerView.Adapter<GroupAdapter.GroupHolder>() {
+class GroupAdapter(private val listener: GroupListener) :
+    RecyclerView.Adapter<GroupAdapter.GroupHolder>() {
     private var data: ArrayList<GroupModel> = ArrayList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GroupHolder {
@@ -25,41 +23,25 @@ class GroupAdapter(private val fragment: GroupFragment, private val viewModel: M
         holder.bind(data[position])
     }
 
-    fun addAll(data: ArrayList<GroupModel>) {
+    fun updateData(data: ArrayList<GroupModel>) {
         this.data.addAll(data)
-    }
-
-    fun clear() {
         this.data.clear()
+        notifyDataSetChanged()
     }
 
     inner class GroupHolder(view: View) : RecyclerView.ViewHolder(view) {
         fun bind(groupModel: GroupModel) {
-            itemView.groupLinearLayout.setOnClickListener {
-                viewModel.chosenGroupElement.value = groupModel
-                fragment.parentFragmentManager.commit {
-                    addToBackStack(null)
-                    setCustomAnimations(
-                        R.anim.zoom_in,
-                        R.anim.zoom_out,
-                        R.anim.parent_zoom_in,
-                        R.anim.parent_zoom_out
-                    )
-                    replace(R.id.fragmentHolder, ChannelFragment.newInstance())
-                }
-            }
-            itemView.turnOnLightButton.setOnClickListener {
-                for (channel in groupModel.channelModelList)
-                    viewModel.turnOnLight(channel.id)
-            }
-
-            itemView.turnOffLightButton.setOnClickListener {
-                for (channel in groupModel.channelModelList)
-                    viewModel.turnOffLight(channel.id)
-            }
-
+            itemView.groupLinearLayout.setOnClickListener { listener.onGroupOpen(groupModel) }
+            itemView.turnOnLightButton.setOnClickListener { listener.onTurnOnLights(groupModel) }
+            itemView.turnOffLightButton.setOnClickListener { listener.onTurnOffLights(groupModel) }
             itemView.headerTextView.text = groupModel.name
             itemView.secondaryTextView.text = groupModel.channelElementsToString()
         }
+    }
+
+    interface GroupListener {
+        fun onGroupOpen(groupModel: GroupModel)
+        fun onTurnOnLights(groupModel: GroupModel)
+        fun onTurnOffLights(groupModel: GroupModel)
     }
 }
