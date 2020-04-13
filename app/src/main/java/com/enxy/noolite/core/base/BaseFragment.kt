084a18e811211abc588237c88eb6758063f1bee3
enxy0 <enxyox@gmail.com>
2020-04-14 00:39:43 +0300

    Add useful functions
    Signed-off-by: enxy0 <enxyox@gmail.com>

--- app/src/main/java/com/enxy/noolite/core/base/BaseFragment.kt
+++ app/src/main/java/com/enxy/noolite/core/base/BaseFragment.kt
@@ -1,15 +1,18 @@
 package com.enxy.noolite.core.base
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import com.enxy.noolite.AndroidApplication
 import com.enxy.noolite.core.di.ApplicationComponent
 import com.enxy.noolite.features.MainActivity
 import kotlinx.android.synthetic.main.toolbar.*
+
 
 abstract class BaseFragment : Fragment() {
     val appComponent: ApplicationComponent by lazy {
@@ -53,6 +56,20 @@ abstract class BaseFragment : Fragment() {
 
     internal fun setToolbarTitle(message: String) {
         toolbar.title = message
+    }
+
+    internal fun close() {
+        parentFragmentManager.popBackStack()
+    }
+
+    internal fun showKeyboardOnView(viewToFocus: View) {
+        viewToFocus.requestFocus()
+        val inputMethodManager =
+            requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.toggleSoftInput(
+            InputMethodManager.SHOW_FORCED,
+            InputMethodManager.HIDE_IMPLICIT_ONLY
+        )
     }
 
     internal fun getMainActivity(): MainActivity = requireActivity() as MainActivity
