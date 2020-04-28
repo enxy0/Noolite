edec85a667a9446fb852bb3ad338165e38cf99e3
enxy0 <enxyox@gmail.com>
2020-04-28 20:55:14 +0300

    Update timeout (set to 1 sec) for connection with server

--- app/src/main/java/com/enxy/noolite/core/di/ApplicationModule.kt
+++ app/src/main/java/com/enxy/noolite/core/di/ApplicationModule.kt
@@ -6,7 +6,9 @@ import com.enxy.noolite.core.network.NetworkService
 import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -20,6 +22,11 @@ class ApplicationModule(private val application: AndroidApplication) {
     @Singleton
     fun provideRetrofit(): Retrofit = Retrofit.Builder()
         .baseUrl(NetworkService.BASE_URL)
+        .client(
+            OkHttpClient().newBuilder()
+                .connectTimeout(1, TimeUnit.SECONDS)
+                .build()
+        )
         .build()
 
     @Provides
