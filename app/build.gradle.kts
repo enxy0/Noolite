05a57051e4af157017dafab8cd67db62d4085456
enxy0 <enxyox@gmail.com>
2025-09-28 00:37:13 +0300

    Release 2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.enxy.noolite"
         minSdk = 23
         targetSdk = 36
-        versionCode = 198
-        versionName = "2.0.3"
+        versionCode = 199
+        versionName = "2.1.0"
     }
 
     signingConfigs {
